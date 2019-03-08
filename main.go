c13752b27069b414fa89e484aa5d55b15e08ae57
Rico Pahlisch <rico.pahlisch@kiwigrid.com>
2019-03-08 11:12:28 +0100

    init commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var VERSION = "latest"
+
+func main() {
+	clientset, err := getClient("")
+	clientset.CoreV1().Pods("").Get("", metav1.GetOptions{})
+}
+
+func getClient(pathToCfg string) (*kubernetes.Clientset, error) {
+	var config *rest.Config
+	var err error
+	if pathToCfg == "" {
+		// in cluster access
+		logrus.Info("Using in cluster config")
+		config, err = rest.InClusterConfig()
+	} else {
+		logrus.Info("Using out of cluster config")
+		config, err = clientcmd.BuildConfigFromFlags("", pathToCfg)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+
+}
